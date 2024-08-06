94f78ba4f0fc32b487ba86d2a2362ce8f3797321
Iurii62Rus <kharlamov.yury2017@yandex.ru>
2024-08-06 19:12:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 first = int(input('Enter first number: '))
 second = int(input('Enter second number: '))
 third = int(input('Enter third number: '))
-if first == second == third:
+
+all_equal = first == second == third
+two_equal = (first == second and not first == third) or (first == third and not first == second) or (second == third and not second == first)
+none_equal = not (first == second or first == third or second == third)
+
+if all_equal:
     print('All numbers are equal. Number of identical numbers: 3')
-elif first == second or first == third or second == third:
+elif two_equal:
     print("At least two numbers are equal. Number of identical numbers: 2")
-else:
+elif none_equal:
     print("There are no equal numbers. Number of identical numbers: 0")
 
 
